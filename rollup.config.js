615cd5060a1eed1c9e6c875b3c39b08478170af6
= <=>
2022-03-14 13:26:55 +0100

    Nothing much

--- rollup.config.js
+++ rollup.config.js
@@ -3,33 +3,32 @@ import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
 import postcss from "rollup-plugin-postcss";
+import external from "rollup-plugin-peer-deps-external";
 import { terser } from "rollup-plugin-terser";
-import peerDepsExternal from "rollup-plugin-peer-deps-external";
 
-const packageJson = require("./package.json");
+const pkg = require("./package.json");
 
 export default [
   {
     input: "src/index.ts",
     output: [
       {
-        file: packageJson.main,
+        file: pkg.main,
         format: "cjs",
         sourcemap: true,
       },
       {
-        file: packageJson.module,
+        file: pkg.module,
         format: "esm",
         sourcemap: true,
       },
     ],
     plugins: [
-      peerDepsExternal(),
+      external(),
       resolve(),
       commonjs(),
       typescript({ tsconfig: "./tsconfig.json" }),
       postcss(),
-
       terser(),
     ],
   },
